8c635e5029ba1dc31c7adb38f5e32c8bb0ac89e5
William Kimak <31807815+wkimak@users.noreply.github.com>
2017-09-21 16:03:48 -0400

    Update wikipedia.js

--- wikipedia-viewer/wikipedia.js
+++ wikipedia-viewer/wikipedia.js
@@ -10,7 +10,7 @@ event.preventDefault();
 
 //animations
 $("h1,h2").fadeOut(400);
-$("#search").animate({bottom: '175px'}, 400);
+$("#search").animate({bottom:'75px'}, 400);
 $("#articles").fadeIn(700);
 
 
@@ -21,9 +21,8 @@ var searchTerm = $("#search").val();
 $("#search").val(" ");
 
 // request url
-//var url = "http://en.wikipedia.org/w/api.php?action=opensearch&search=searchTerm&format=json&callback=?";
 var offset = 0;
-var url = `https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${searchTerm}&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=30prop=info&pageids=14640471&inprop=url`;
+var url = `https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${searchTerm}&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=100prop=info&pageids=14640471&inprop=url`;
 
 //data
 var options = {
@@ -34,7 +33,7 @@ search: searchTerm,
 function showArticles(data){
 	var showHTML = "<ul class='ul-style'>";
 	$.each(data.query.search, function(index, term){
-     showHTML += '<li class="li-style"> <a href="#"' + term.pageid + '"<h2>' + term.title + '</h2> </a>';
+     showHTML += "<li class='li-style'> <a href='https://en.wikipedia.org/wiki/" + term.title + "'target='_blank'> <h2>" + term.title + "</h2> </a>";
      showHTML += " <p>" + term.snippet + "</p> </li>";
 	}); //each loop
 showHTML += "</ul>";
