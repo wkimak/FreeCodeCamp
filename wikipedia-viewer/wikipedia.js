74e964406b09bb86659da7bae28a0c3d29e35502
William Kimak <31807815+wkimak@users.noreply.github.com>
2017-11-21 00:58:12 -0500

    Update wikipedia.js

--- wikipedia-viewer/wikipedia.js
+++ wikipedia-viewer/wikipedia.js
@@ -33,10 +33,10 @@ search: searchTerm,
 function showArticles(data){
 	var showHTML = "<ul class='ul-style'>";
 	$.each(data.query.search, function(index, term){
-     showHTML += "<li class='li-style'> <a href='https://en.wikipedia.org/wiki/" + term.title + "'target='_blank'> <h2>" + term.title + "</h2> </a>";
-     showHTML += " <p>" + term.snippet + "</p> </li>";
+          showHTML += "<li class='li-style'> <a href='https://en.wikipedia.org/wiki/" + term.title + "'target='_blank'> <h2>" + term.title + "</h2> </a>";
+          showHTML += " <p>" + term.snippet + "</p> </li>";
 	}); //each loop
-showHTML += "</ul>";
+          showHTML += "</ul>";
 	$("#articles").html(showHTML);
 };
 
