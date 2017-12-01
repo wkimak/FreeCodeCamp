e61b63768c77ef9cda9d16c89e0e81370f8a5165
William Kimak <31807815+wkimak@users.noreply.github.com>
2017-12-01 14:12:55 -0500

    Update toe.js

--- tac-tac-toe/toe.js
+++ tac-tac-toe/toe.js
@@ -29,56 +29,21 @@ var playerContainer = document.getElementById("player-container");
 
 
   /* ----------------------- GamePlay ----------------------------------------------------- */
-
-
 // ----------------- Start Game ------------------ //
 
-
-// function that decides the winner
-function winner(){
-
-  //victory cells
-  var victory = [[cell0,cell1,cell2], [cell3,cell4,cell5], [cell6,cell7,cell8], [cell0,cell3,cell6], 
-                [cell1,cell4,cell7],  [cell2,cell5,cell8], [cell0,cell4,cell8], [cell2,cell4,cell6]]; 
-
-for(var i = 0; i <= 7; i++){
-if(xArray.includes(victory[i][0]) && xArray.includes(victory[i][1]) && xArray.includes(victory[i][2])){
-    alert("You Win!");
-    var reload = setTimeout(function(){
-      location.reload();
-    },1500);
-    break;  
-  } else if(oArray.includes(victory[i][0]) && oArray.includes(victory[i][1]) && oArray.includes(victory[i][2])){
-    alert("You Lose");
-    var reload = setTimeout(function(){
-      location.reload();
-    },1500);
-    break;
-  } else if(cellsArray.length == 0){
-    alert("Tie Game");
-    var reload = setTimeout(function(){
-      location.reload();
-    },1500);
-    break;
-  }
-}
-}
 
 // players object
 var players = {
   player1: function(){
-      var x = document.createElement("IMG");
-      x.setAttribute("src", "http://cdn.mysitemyway.com/etc-mysitemyway/icons/legacy-previews/icons/magic-marker-icons-alphanumeric/114602-magic-marker-icon-alphanumeric-letter-x.png");
-      x.setAttribute("width", "100");
-      x.setAttribute("height", "auto");
+      var x = document.createElement("SPAN");
+      x.setAttribute("width", "200px");
+      x.innerHTML = "<span class='playerText'>X</span>";
       return x;
   },
 
   player2: function(){
-      var o = document.createElement("IMG");
-      o.setAttribute("src", "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/Letter_o.svg/2000px-Letter_o.svg.png");
-      o.setAttribute("width", "100");
-      o.setAttribute("height", "auto");
+      var o = document.createElement("SPAN");
+      o.innerHTML = "<span class='playerText'>O</span>";
       return o;
   }
 };
@@ -86,23 +51,30 @@ var players = {
 
 // ----------- MAIN Moves function -------------- //
 function move(){
-
+console.log(cellsArray.length);
 function x(){
+ 
 //append "x" to event.target 
- event.target.appendChild(players.player1());
+if(event.target.style.backgroundColor != "rgb(102, 198, 144)" && event.target.textContent != "X" && event.target.style.backgroundColor != "rgb(255,132,141)" && event.target.textContent != "O"){
+event.target.appendChild(players.player1());
+event.target.className="x";
+event.target.style.backgroundColor = "rgb(102,198,144)";
+
 //push removed event.target index into xArray
 xArray.push(cellsArray.splice(cellsArray.indexOf(event.target),1));
 //flatten xArray
 xArray = xArray.reduce(function(prev, next){
   return prev.concat(next);
 },[]);
-console.log(xArray);
+}
 }
 
 function o(){
 //if cellArray length is more than 1, append player 2
 if(cellsArray.length > 1){
 cellsArray[0].appendChild(players.player2());
+cellsArray[0].className = "o";
+cellsArray[0].style.backgroundColor = "rgb(255,132,141)";
 //push removed cellsArray index into oArray
 oArray.push(cellsArray.splice(0,1));
 //flatten oArray
@@ -118,6 +90,71 @@ o();
 winner();
 }
 
-
 //Cells event listener
 cells.addEventListener("click", move);
+
+// function that decides the winner
+function winner(){
+
+  //victory cells
+  var victory = [[cell0,cell1,cell2], [cell3,cell4,cell5], [cell6,cell7,cell8], [cell0,cell3,cell6], 
+                [cell1,cell4,cell7],  [cell2,cell5,cell8], [cell0,cell4,cell8], [cell2,cell4,cell6]]; 
+
+for(var i = 0; i <= 7; i++){
+if(xArray.includes(victory[i][0]) && xArray.includes(victory[i][1]) && xArray.includes(victory[i][2])){
+    alert("You Win!");
+     var reload = setTimeout(function(){
+      location.reload();
+    },2000);
+    victory[i][0].style.backgroundColor = "darkgreen";
+    victory[i][1].style.backgroundColor = "darkgreen";
+    victory[i][2].style.backgroundColor = "darkgreen";
+    break;  
+  } else if(oArray.includes(victory[i][0]) && oArray.includes(victory[i][1]) && oArray.includes(victory[i][2])){
+    alert("You Lose");
+    victory[i][0].style.backgroundColor = "darkred";
+    victory[i][1].style.backgroundColor = "darkred";
+    victory[i][2].style.backgroundColor = "darkred";
+    var reload = setTimeout(function(){
+      location.reload();
+    },2000);
+    break;
+  } else if(cellsArray.length == 0 && !xArray.includes(victory[i][0]) && !xArray.includes(victory[i][1]) && !xArray.includes(victory[i][2] && !oArray.includes(victory[i][0]) && !oArray.includes(victory[i][1]) && !oArray.includes(victory[i][2]))){
+    alert("Tie Game");
+    var reload = setTimeout(function(){
+      location.reload();
+    },2000);
+    break;
+  } 
+}
+
+}
+
+/*
+switch(true){
+case  cell0.className === cell1.className && cell1.className === cell2.className:
+alert("Player" + cell3.textContent + " Won");
+break;
+case  cell3.className === cell4.className && cell4.className === cell5.className:
+alert("Player" + cell3.textContent + " Won");
+break;
+case  cell6.className === cell7.className && cell7.className === cell8.className:
+alert("Player" + cell6.textContent + " Won");
+break;
+case  cell0.className === cell3.className && cell3.className === cell6.className:
+alert("Player" + cell0.textContent + " Won");
+break;
+case  cell1.className == cell4.className && cell4.className == cell7.className:
+alert("Player" + cell1.textContent + " Won");
+break;
+case  cell2.className == cell5.className && cell5.className == cell8.className:
+alert("Player" + cell2.textContent + " Won");
+break;
+case  cell0.className == cell4.className && cell4.className == cell8.className:
+alert("Player" + cell0.textContent + " Won");
+break;
+case  cell2.className == cell4.className && cell4.className == cell6.className:
+alert("Player" + cell2.textContent + " Won");
+break;
+}
+*/
